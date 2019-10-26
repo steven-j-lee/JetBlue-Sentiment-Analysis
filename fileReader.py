61139386f81ae41901c8527211540bea69d2fdc0
Steven Lee <koreancoffe@gmail.com>
2019-10-26 13:03:00 -0400

    .

--- fileReader.py
+++ fileReader.py
@@ -4,9 +4,10 @@ from nltk.corpus import subjectivity
 from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
 from pprint import pprint
-from sklearn.feature_extraction.text import CountVectorizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
-#sia = SIA()
+
+sia = SIA()
 result = []
 
 with open('quotes.json', 'r') as json_file:
@@ -16,3 +17,20 @@ data_length = len(json_data)
 
 #test for json
 print (json_data[0])
+
+
+#sia.all_words(json_data)
+for review in json_data:
+    opinions = sia.polarity_scores(review)
+    opinions['text'] = review
+    results.append(opinions)
+    print (review['text'])
+
+#pprint (results[:3], width = 500)
+#sentences=["You guys suck!"]
+
+#for sentence in sentences:
+#    ss = sia.polarity_scores(sentence)
+#    result.append(ss)
+
+#pprint(result[:1],width=100)
