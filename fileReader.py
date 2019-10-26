e746d004ae2f2c7a83603e4df647ed7cbbd978c8
Steven Lee <koreancoffe@gmail.com>
2019-10-26 08:36:38 -0400

    .

--- fileReader.py
+++ fileReader.py
@@ -1,1 +1,18 @@
 import json
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import subjectivity
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+from pprint import pprint
+from sklearn.feature_extraction.text import CountVectorizer
+
+#sia = SIA()
+result = []
+
+with open('quotes.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
+data_length = len(json_data)
+
+#test for json
+print (json_data[0])
