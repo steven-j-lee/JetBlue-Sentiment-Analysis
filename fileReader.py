0327671f0aac7dedc19b08e1efd378883beda35d
Steven Lee <koreancoffe@gmail.com>
2019-10-26 13:20:57 -0400

    sentiment analyzer script

--- fileReader.py
+++ fileReader.py
@@ -4,10 +4,10 @@ from nltk.corpus import subjectivity
 from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
 from pprint import pprint
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-sia = SIA()
+sia = SentimentIntensityAnalyzer()
 result = []
 
 with open('quotes.json', 'r') as json_file:
@@ -16,17 +16,22 @@ with open('quotes.json', 'r') as json_file:
 data_length = len(json_data)
 
 #test for json
-print (json_data[0])
+#print (json_data[0])
 
 
 #sia.all_words(json_data)
 for review in json_data:
-    opinions = sia.polarity_scores(review)
-    opinions['text'] = review
-    results.append(opinions)
-    print (review['text'])
+#    opinions = sia.polarity_scores(review)
+#    opinions['text'] = review
+    opinions = sia.polarity_scores(review['text'])
+    opinions['review'] = review
+    result.append(opinions)
+# result.append(sia.polarity_scores(review['text']))
+#    print (review['text'])
 
-#pprint (results[:3], width = 500)
+pprint (result[:1], width = 500)
+pprint (result[:4], width = 500)
+#pprint (result[:5], width = 500)
 #sentences=["You guys suck!"]
 
 #for sentence in sentences:
