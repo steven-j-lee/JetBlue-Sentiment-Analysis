d947773337a5d5ccadef81284c0901c9d14c153f
Filippo Baronello <16783537+fello252@users.noreply.github.com>
2019-10-27 05:26:37 -0400

    Update update.sh

--- tutorial/spiders/update.sh
+++ tutorial/spiders/update.sh
@@ -4,3 +4,8 @@ echo $(date --rfc-3339=seconds | cut -d- -f1-3) > lastupdate
 mv ../fileupdate.py .
 python3 filechanger.py
 
+echo "<!DOCTYPE HTML>" > ../../web/words.html
+echo "<head><title> </title> <head>" >> ../../web/words.html
+echo "<body>" >> ../../web/words.html 
+cat Wordreview.csv >> ../../web/words.html 
+echo "</body>" >> ../../web/words.html 
