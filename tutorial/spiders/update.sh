b0c743b8ef9526eca6478d5d1d35e18c727298ed
Steven Lee <koreancoffe@gmail.com>
2019-10-27 05:54:22 -0400

    final changes

--- tutorial/spiders/update.sh
+++ tutorial/spiders/update.sh
@@ -5,7 +5,9 @@ mv ../fileupdate.py .
 python3 filechanger.py
 
 echo "<!DOCTYPE HTML>" > ../../web/words.html
-echo "<head><title> </title> <head>" >> ../../web/words.html
-echo "<body>" >> ../../web/words.html 
-cat Wordreview.csv >> ../../web/words.html 
-echo "</body>" >> ../../web/words.html 
+echo "<head><title> Most Associated Words </title>
+<link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
+  <link rel = "stylesheet" href="../../web/style.css"> <head>" >> ../../web/words.html
+echo "<body>" >> ../../web/words.html
+cat Wordreview.csv >> ../../web/words.html
+echo "</body>" >> ../../web/words.html
