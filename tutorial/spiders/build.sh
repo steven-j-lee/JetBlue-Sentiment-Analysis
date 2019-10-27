e809678aab4305d355ac3186196b203105d45cb1
Filippo Baronello <16783537+fello252@users.noreply.github.com>
2019-10-27 02:00:01 -0400

    Update build.sh

--- tutorial/spiders/build.sh
+++ tutorial/spiders/build.sh
@@ -4,4 +4,6 @@ mv filereader.py ..
 rm aquote.json
 scrapy crawl aqual -o aquote.json
 twint -s jetblue --since "2014-10-26 20:30:15" -pt -o test-cases.csv --csv
+mv ../filereader.py .
+python3 filereader.py
 
